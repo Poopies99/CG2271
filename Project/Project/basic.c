a7c529fbe21c2cab30f3e680bd62a991f80b7a96
Topher <69495787+Poopies99@users.noreply.github.com>
2021-12-21 12:32:48 +0800

    Delete Project/Project directory

--- Project/Project/basic.c
+++
@@ -1,5 +0,0 @@
-#include "basic.h"
-
-osSemaphoreId_t brainSem, audioSem, motorSem;
-
-
--- Project/Project/basic.h
+++
@@ -1,8 +0,0 @@
-#include "MKL25Z4.h"
-#include "RTE_Components.h"
-#include  CMSIS_device_header
-#include "cmsis_os2.h"
-
-extern osSemaphoreId_t brainSem;
-extern osSemaphoreId_t audioSem;
-extern osSemaphoreId_t motorSem;
